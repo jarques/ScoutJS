b0f234fd9c3bb9f750d2ca29a2dc8cca764d7089
meltingice <meltingice8917@gmail.com>
2011-03-31 15:03:11 -0400

    Added watch() method and allow for geolocation options to be passed through

--- scout.js
+++ scout.js
@@ -3,18 +3,66 @@
     Created by @jarques and @meltingice
 */
 (function(){
+
+  var geoCapable = (function () {
+    return !!navigator.geolocation;
+  }());
   
   var scout = (function (method) {
+    var self = this;
+    
     return {
-      get: function(coords){
-        if (navigator.geolocation) {
-            return navigator.geolocation.getCurrentPosition(
-                function(location) {coords(location.coords)},
-                function(){return false;}
-            );
-        } else {
-            return false;
+      get: function(){
+        if (!geoCapable) { return; }
+        
+        var opts = {};
+        var callback = function () {};
+        
+        if (arguments.length == 1) {
+          callback = arguments[0];
+        } else if (arguments.length == 2) {
+          opts = arguments[0];
+          callback = arguments[1];
         }
+        
+        return navigator.geolocation.getCurrentPosition(
+          // Success  
+          function (location) {
+            callback.call(self, location.coords);
+          },
+          
+          // Error
+          function () {},
+          
+          // Options
+          opts
+        );
+      },
+      
+      watch: function () {
+        if (!geoCapable) { return; }
+        
+        var opts = {};
+        var callback = function () {};
+        
+        if (arguments.length == 1) {
+          callback = arguments[0];
+        } else if (arguments.length == 2) {
+          opts = arguments[0];
+          callback = arguments[1];
+        }
+        
+        return navigator.geolocation.watchPosition(
+          function (position) {
+            callback.call(self, position.coords);
+          },
+          
+          // Error
+          function () {},
+          
+          // Options
+          opts
+        );
       }
     };
   }());
