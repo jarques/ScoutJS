6480533316b6e31a6969f8fac9009ac6b813b607
Jarques <hello@jpretorius.com>
2011-03-31 14:13:18 -0700

    readme changes, removed version number for now.

--- scout.js
+++ scout.js
@@ -1,71 +1,73 @@
 /*
-    Scout.js v1
+    Scout.js
     Created by @jarques and @meltingice
 */
-(function(){
+(function() {
 
-  var geoCapable = (function () {
-    return !!navigator.geolocation;
-  }());
-  
-  var scout = (function (method) {
-    var self = this;
-    
-    return {
-      get: function(){
-        if (!geoCapable) { return; }
-        
-        var opts = {};
-        var callback = function () {};
-        
-        if (arguments.length == 1) {
-          callback = arguments[0];
-        } else if (arguments.length == 2) {
-          opts = arguments[0];
-          callback = arguments[1];
-        }
-        
-        return navigator.geolocation.getCurrentPosition(
-          // Success  
-          function (location) {
-            callback.call(self, location.coords);
-          },
-          
-          // Error
-          function () {},
-          
-          // Options
-          opts
-        );
-      },
-      
-      watch: function () {
-        if (!geoCapable) { return; }
-        
-        var opts = {};
-        var callback = function () {};
-        
-        if (arguments.length == 1) {
-          callback = arguments[0];
-        } else if (arguments.length == 2) {
-          opts = arguments[0];
-          callback = arguments[1];
-        }
-        
-        return navigator.geolocation.watchPosition(
-          function (position) {
-            callback.call(self, position.coords);
-          },
-          
-          // Error
-          function () {},
-          
-          // Options
-          opts
-        );
-      }
-    };
-  }());
-  
-  window.scout = scout;
+	var geoCapable = (function() {
+		return !!navigator.geolocation;
+	} ());
+
+	var scout = (function(method) {
+		var self = this;
+
+		return {
+			get: function() {
+				if (!geoCapable) {
+					return;
+				}
+
+				var opts = {};
+				var callback = function() {};
+
+				if (arguments.length == 1) {
+					callback = arguments[0];
+				} else if (arguments.length == 2) {
+					opts = arguments[0];
+					callback = arguments[1];
+				}
+
+				return navigator.geolocation.getCurrentPosition(
+				// Success  
+				function(location) {
+					callback.call(self, location.coords);
+				},
+
+				// Error
+				function() {},
+
+				// Options
+				opts);
+			},
+
+			watch: function() {
+				if (!geoCapable) {
+					return;
+				}
+
+				var opts = {};
+				var callback = function() {};
+
+				if (arguments.length == 1) {
+					callback = arguments[0];
+				} else if (arguments.length == 2) {
+					opts = arguments[0];
+					callback = arguments[1];
+				}
+
+				return navigator.geolocation.watchPosition(
+				function(position) {
+					callback.call(self, position.coords);
+				},
+
+				// Error
+				function() {},
+
+				// Options
+				opts);
+			}
+		};
+	} ());
+
+	window.scout = scout;
 })();
