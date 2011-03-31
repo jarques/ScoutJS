46039492e8aa7eed8e55d30bf4844f26673b4f5c
meltingice <meltingice8917@gmail.com>
2011-03-31 14:38:35 -0400

    Removed jQuery dependency

--- scout.js
+++ scout.js
@@ -1,18 +1,23 @@
 /*
     Scout.js v1
-    Created by @jarques
+    Created by @jarques and @meltingice
 */
-(function($){
-	$.scout = {        
-		get:function(coords){
-			if (navigator.geolocation) {
-			    navigator.geolocation.getCurrentPosition(
-			        function(location) {coords(location.coords)},
-			        function(){return false;}
-			    );
-			} else {
-			    return false;
-			}
-		}
-	}
-})(jQuery);
+(function(){
+  
+  var scout = (function (method) {
+    return {
+      get: function(coords){
+        if (navigator.geolocation) {
+            return navigator.geolocation.getCurrentPosition(
+                function(location) {coords(location.coords)},
+                function(){return false;}
+            );
+        } else {
+            return false;
+        }
+      }
+    };
+  }());
+  
+  window.scout = scout;
+})();
